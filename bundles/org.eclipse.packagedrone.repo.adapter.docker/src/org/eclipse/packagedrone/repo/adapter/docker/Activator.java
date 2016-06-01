709c907db50dcec2f71181ac44171d2645905307
Pubudu Fernando <pubudu.12@cse.mrt.ac.lk>
2016-06-02 02:35:56 +0530

    adapter.docker bundle added and get loaded and active in the OSGi environment successfully

---
+++ bundles/org.eclipse.packagedrone.repo.adapter.docker/src/org/eclipse/packagedrone/repo/adapter/docker/Activator.java
@@ -0,0 +1,33 @@
+package org.eclipse.packagedrone.repo.adapter.docker;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator
+{
+    private final Logger log = Logger.getLogger ( "Docker Adapter" );
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start ( final BundleContext context ) throws Exception
+    {
+        this.log.log ( Level.INFO, "Hello World" );
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop ( final BundleContext context ) throws Exception
+    {
+        this.log.log ( Level.INFO, "Goodbye World" );
+    }
+
+}
